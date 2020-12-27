cdccb5b7088e7afe61d700a40a9a129d9ae6735f
unknown <unenes.viejos@gmail.com>
2020-12-27 12:26:56 -0600

    Creacion del metodo tirarDados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,18 @@ export class AppComponent {
 
   dadoIzquierdo = "../assets/img/dice1.png";
   dadoDerecho = "../assets/img/dice4.png";
+ 
+  numero1 = 0;
+  numero2 = 0;
+  
+  tirarDados() : void {
+    this.numero1 = Math.random();
+    this.numero2 = Math.random();
+    console.log(this.numero1);
+    console.log(this.numero2);
+
+  }
+
+
 
 }
